15fe126150d461fed08172973f1ca88b824ceb71
IK Song <lcdf17@gmail.com>
2020-09-07 16:44:12 -0500

    added app clip

---
+++ Clip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clip
+//
+//  Created by Serge Gainsbourg on 9/7/20.
+//  Copyright © 2020 IKSong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome To Clip")
+            .font(.headline)
+            .foregroundColor(Color.purple)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
